067fa6151904919beab5e5f8a0a811bb17a877ea
Matias Ruiz <ruizmat@gmail.com>
2022-06-15 18:01:35 +0200

    Adding register controller to save user data to the database

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -13,13 +16,41 @@ class RegisterController extends Controller
     }
     public function store(Request $request)
     {
-        // dd($request);
+        
         // dd($request->get('email'));
+
+        // Modifying request to validate username properly
+        $request->request->add(['username'=> Str::slug($request->username)]);
+        // dd($request);
+
         $this->validate($request, [
           'name' => 'required|max:50',
           'username' => 'required|unique:users|min:3|max:20',
           'email' => 'required|unique:users|email|max:60',
-          'password' => 'required'
+          'password' => 'required|confirmed|min:6'
         ]);
+
+        // dd('Creando usuario');
+
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make( $request->password ) 
+        ]);
+
+        // Autenticar
+        // auth()->attempt([
+        //     'email' => $request->email,
+        //    'password' => $request->password
+        //]);
+
+        // Otra forma de autenticar
+        auth()->attempt($request->only('email', 'password'));
+
+        // Redireccionar
+        return redirect()->route('posts.index');
+
     }
 }

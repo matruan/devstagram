a134831cd8228b34dca8bc0d6bfbc8276159f59b
Matias Ruiz <ruizmat@gmail.com>
2022-06-22 18:08:03 +0200

    Adding ImagenCotroller using InterventionImage (php) and Dropzone (js) libraries, validating posts and creating factory to test them

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,15 +4,24 @@
   Crea una nueva publicacion
 @endsection
 
+@push('styles')
+  <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+@endpush
+
 @section('contenido')
     <div class="md:flex md:items-center">
         <div class="md:w-1/2 px-10">
-           <form action="/imagenes" id="dropzone" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
+           <form action="{{ route('imagenes.store') }}" 
+             id="dropzone" 
+             method="POST"  
+             enctype="multipart/form-data"
+             class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
+               @csrf
 
            </form>
         </div>
         <div class="md:w-1/2 p-10 bg-white rounded-lg shadow-xl mt-10 md:mt-0">
-            <form action="{{ route('register') }}" method="POST" novalidate>
+            <form action="{{ route('posts.store') }}" method="POST" novalidate>
                 @csrf
                 <div class="mb-5">
                   <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">
@@ -40,10 +49,22 @@
                       name="descripcion"
                       placeholder="Descripción de la Publicación"
                       class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
-                    >{{ old('titulo') }}</textarea>
-                    @error('titulo')
+                    >{{ old('descripcion') }}</textarea>
+                    @error('descripcion')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
                     @enderror
+                  </div>
+
+                  <div class="mb-5">
+                    <input 
+                      name="imagen"
+                      type="hidden"
+                      value="{{ old('imagen') }}"
+                    />
+                    @error('imagen')
+                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+
                   </div>
 
                   <input 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
@@ -30,4 +31,6 @@ Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
+Route::post('/imagenes', [ImagenController::class, 'store' ])->name('imagenes.store');

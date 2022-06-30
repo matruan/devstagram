f4a7065c30172f96c68700bbca3ec7dcbab06a71
Matias Ruiz <ruizmat@gmail.com>
2022-06-30 17:26:26 +0200

    Setting up controller in order to store comments, modified show.blade view to show result message after storing the comment

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -2,21 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comentario;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ComentarioController extends Controller
 {
-    public function store()
+    public function store(Request $request, User $user, Post $post)
     {
-        dd('Comentando...');
-        
+       
         // Validar
-        
+        $this->validate($request, [
+          'comentario' => 'required|max:255'
+
+        ]);
+
         // Almacenar
+        Comentario::create([
+            'user_id' => auth()->user()->id,
+            'post_id' => $post->id,
+            'comentario' => $request->comentario
+        ]);
 
         // Imprimir mensaje
-
-
-
+        return back()->with('mensaje', 'Comentario realizado correctamente');
     }
 }
+

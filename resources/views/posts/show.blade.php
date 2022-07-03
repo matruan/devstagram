349e710e258cbb92ea438d0c9558c23329a89286
Matias Ruiz <ruizmat@gmail.com>
2022-07-03 21:49:13 +0200

    Implemented destroy method in the Like controller through implemented likes method relationship in the User model and showing properly the like icon in the show.blade view using checkLikes new function in the Post model

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,18 +10,40 @@
       <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
 
       <div class="p-3 flex items-center gap-4">
-        <form method="POST" action="{{ route('posts.likes.store', $post) }}">
-          @csrf
-          <div class="my-4">
-            <button type="submit">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="white" viewBox="0 0 24 24"
-                stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-              </svg>
-            </button>
-          </div>
-        </form>
+
+        @auth
+
+          @if ($post->checkLike(auth()->user()))
+            <form method="POST" action="{{ route('posts.likes.destroy', $post) }}">
+              @method('DELETE')
+              @csrf
+              <div class="my-4">
+                <button type="submit">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="red" viewBox="0 0 24 24"
+                    stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                  </svg>
+                </button>
+              </div>
+            </form>
+          @else
+            <form method="POST" action="{{ route('posts.likes.store', $post) }}">
+              @csrf
+              <div class="my-4">
+                <button type="submit">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="white" viewBox="0 0 24 24"
+                    stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                  </svg>
+                </button>
+              </div>
+            </form>
+          @endif
+
+
+        @endauth
 
         <p>0 Likes</p>
       </div>

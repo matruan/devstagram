a5bd74bb95f259ba8de5510f3b4d9785fabe2722
Matias Ruiz <ruizmat@gmail.com>
2022-07-04 11:20:03 +0200

    Showing number of likes in show.blade view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -45,7 +45,9 @@
 
         @endauth
 
-        <p>0 Likes</p>
+        <p class="font-bold">{{ $post->likes->count() }}
+          <span class="font-normal"> Likes </span>
+        </p>
       </div>
 
       <div>

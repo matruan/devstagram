175793a33106e4b258ef1d229d1d6ae0a42b16ed
Matias Ruiz <ruizmat@gmail.com>
2022-07-04 11:48:56 +0200

    Adding pencil icon to edit user profile in dashboard.blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,57 +1,72 @@
 @extends('layouts.app')
 
 @section('titulo')
-    Perfil: {{ $user->username }}
+  Perfil: {{ $user->username }}
 @endsection
 
 @section('contenido')
 
-    <div class="flex justify-center">
-        <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
-            <div class="w-8/12 lg:w-6/12 px-5">
-                <img src="{{ asset('img/usuario.svg') }}" alt="imagen usuario" />
-            </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py-10 md:py-10">
-                <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
+  <div class="flex justify-center">
+    <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
+      <div class="w-8/12 lg:w-6/12 px-5">
+        <img src="{{ asset('img/usuario.svg') }}" alt="imagen usuario" />
+      </div>
+      <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py-10 md:py-10">
 
-                <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
-                    0
-                    <span class="font-normal">Seguidores</span>
-                </p>
+        <div class="flex items-center gap-2">
+          <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
 
-                <p class="text-gray-800 text-sm mb-3 font-bold">
-                    0
-                    <span class="font-normal">Siguiendo</span>
-                </p>
+          @auth
+            @if ($user->id === auth()->user()->id)
+              <a class="text-gray-500 hover:text-gray-600 cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path
+                    d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                </svg>
 
-                <p class="text-gray-800 text-sm mb-3 font-bold">
-                    0
-                    <span class="font-normal">Posts</span>
-                </p>
-            </div>
-
+              </a>
+            @endif
+          @endauth
         </div>
-    </div>
-            
-    <section class="container mx-auto mt-10">
-      <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
-        @if ($posts->count())
-          <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            @foreach ($posts as $post)
-              <div>
-                <a href="{{ route('posts.show', [ 'post' => $post, 'user' => $user ] ) }}">
-                  <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
-                </a>             
-              </div>
-            @endforeach
-          </div>
 
-          <div class="my-10">
-            {{ $posts->links('pagination::tailwind') }}  
+        <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
+          0
+          <span class="font-normal">Seguidores</span>
+        </p>
+
+        <p class="text-gray-800 text-sm mb-3 font-bold">
+          0
+          <span class="font-normal">Siguiendo</span>
+        </p>
+
+        <p class="text-gray-800 text-sm mb-3 font-bold">
+          0
+          <span class="font-normal">Posts</span>
+        </p>
+      </div>
+
+    </div>
+  </div>
+
+  <section class="container mx-auto mt-10">
+    <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
+    @if ($posts->count())
+      <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        @foreach ($posts as $post)
+          <div>
+            <a href="{{ route('posts.show', ['post' => $post, 'user' => $user]) }}">
+              <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
+            </a>
           </div>
-        @else
-          <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
-        @endif
-    </section>
+        @endforeach
+      </div>
+
+      <div class="my-10">
+        {{ $posts->links('pagination::tailwind') }}
+      </div>
+    @else
+      <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
+    @endif
+  </section>
 
 @endsection

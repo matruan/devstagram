4ae834423fd20b3dafd920cc37a4d8cf28804937
Matias Ruiz <ruizmat@gmail.com>
2022-07-04 18:00:02 +0200

    Implementing store function in PerfilController and generating migration to store images in User table

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PerfilController extends Controller
 {
@@ -13,11 +16,41 @@ class PerfilController extends Controller
 
     public function index()
     {
-       return view('perfil.index');
+        return view('perfil.index');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        dd('Guardando cambios');
+        $request->request->add(['username' => Str::slug($request->username)]);
+
+        $this->validate($request, [
+            'username' => ['required', 'unique:users,username,' . auth()->user()->id, 'min:3', 'max:20', 'not_in:twitter,editar-perfil'],
+        ]);
+
+        if ($request->imagen) {
+            $imagen = $request->file('imagen');
+
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+    
+            $imagenServidor = Image::make($imagen);
+    
+            $imagenServidor->fit(1000, 1000);
+            
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+            
+            $imagenServidor->save($imagenPath);
+    
+        }
+
+        // Guardar cambios
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? '';
+        $usuario->save();
+
+        // Redireccionar
+        return redirect()->route('posts.index', $usuario->username);
+
+
     }
 }

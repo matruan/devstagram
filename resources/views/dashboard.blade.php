ee0e7439ca181520a04d54dd0b0b459d3670fa4a
Matias Ruiz <ruizmat@gmail.com>
2022-07-05 13:23:31 +0200

    User image will show selected user image or default

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,8 @@
   <div class="flex justify-center">
     <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
       <div class="w-8/12 lg:w-6/12 px-5">
-        <img src="{{ asset('img/usuario.svg') }}" alt="imagen usuario" />
+        <img src="{{ $user->imagen ? asset('perfiles') . '/' . $user->imagen : asset('img/usuario.svg') }}"
+          alt="imagen usuario" />
       </div>
       <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py-10 md:py-10">
 
@@ -18,10 +19,7 @@
 
           @auth
             @if ($user->id === auth()->user()->id)
-              <a  
-                href="{{ route('perfil.index') }}"
-                class="text-gray-500 hover:text-gray-600 cursor-pointer"
-                >
+              <a href="{{ route('perfil.index') }}" class="text-gray-500 hover:text-gray-600 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                   <path
                     d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />

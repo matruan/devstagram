7054fc36d8469be5d33a61ff955742fe7f2abd64
Matias Ruiz <ruizmat@gmail.com>
2022-07-11 20:19:30 +0200

    Paginating posts in home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-      return view('home');
+      // Get users that authenticated user is following
+      $ids = auth()->user()->following->pluck('id')->toArray();
+      $posts = Post::whereIn('user_id', $ids)->paginate(20);
+
+      return view('home', [
+        'posts' => $posts
+      ]);
     }
 }
